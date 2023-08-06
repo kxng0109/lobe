81b3075eb4e0011fb4b0ca2af67b47f3885e3283
kxng0109 <iluhjosh13@gmail.com>
2023-08-06 22:23:34 +0100

    I am done, it just remains the iframe part

---
+++ index.js
@@ -0,0 +1,20 @@
+let menuBtn = document.querySelector('#header-menu');
+let openHamburgerBtn = document.querySelector("#open--hamburger");
+let closeHamburgerBtn = document.querySelector("#close--hamburger");
+let headerExtension = document.querySelector('#header-extension');
+let extensionLinks = document.querySelectorAll('.he');
+let video = document.querySelector('#main--video');
+
+menuBtn.onclick = () =>{
+	if (getComputedStyle(openHamburgerBtn).display === 'inline-block') {
+		headerExtension.style.top = '80px';
+		openHamburgerBtn.style.display = 'none';
+		closeHamburgerBtn.style.display = 'inline-block';
+		extensionLinks.forEach(item => item.style.opacity = '1');
+	} else{
+		headerExtension.style.top = '-485px'
+		openHamburgerBtn.style.display = 'inline-block';
+		closeHamburgerBtn.style.display = 'none';
+		extensionLinks.forEach(item => item.style.opacity = '0');
+	}
+}
